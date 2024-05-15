9d46666ccce880497a7e3bb698b2db1070b3622a
nemanja-1995 <81355934+nemanja-1995@users.noreply.github.com>
2024-05-15 11:06:01 +0200

    Reimplemented InputWithTitle component.

--- app/[locales]/components/InputWithTitle.tsx
+++ app/[locales]/components/InputWithTitle.tsx
@@ -1,4 +1,6 @@
-import React, { ChangeEvent, ReactNode } from "react";
+'use client';
+import React, { ChangeEvent, ReactNode, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 interface InputWithTitleProps {
   titleLeft: string;
@@ -19,9 +21,15 @@ interface InputWithTitleProps {
 
 const InputWithTitle: React.FC<InputWithTitleProps> = ({ titleLeft, iconRight, bottomTextRight = "", bottomTextLeft = "", value, setValue, 
   type, validated, inputStyle, titleStyle, placeholderTxt, bTxtRightStyle, bTxtLeftStyle, inputDisabled = false }) => {
+  
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
   };
 
   return (
@@ -30,10 +38,21 @@ const InputWithTitle: React.FC<InputWithTitleProps> = ({ titleLeft, iconRight, b
         {titleLeft && <span className={`label-text font-bold ${titleStyle}`}>{titleLeft}</span>}
         {iconRight && <span className="label-text-alt cursor-pointer">{iconRight}</span>}
       </div>
-      <input placeholder={placeholderTxt || "Type here..."} 
-        className={`input border-2 ${!validated ? "input-error" : "input-bordered"} w-full ${
-        !validated ? "border-invalid focus:border-invalid" : "border-gray-light focus:border-gray-light"}`}
-        type={type} value={value} onChange={handleChange} disabled={inputDisabled}/>
+      <div className="relative">
+        <input placeholder={placeholderTxt || "Type here..."} 
+          className={`input border-2 ${!validated ? "input-error" : "input-bordered"} w-full ${
+          !validated ? "border-invalid focus:border-invalid" : "border-gray-light focus:border-gray-light"}`}
+          type={type === 'password' ? (showPassword ? "text" : "password") : type} value={value} 
+          onChange={handleChange} onBlur={() => setShowPassword(false)} disabled={inputDisabled} />
+        {type === 'password' && 
+        <button
+          type="button"
+          className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+          onClick={togglePasswordVisibility}
+        >
+          {showPassword ? <FaEyeSlash className="h-5 w-5 text-gray-400" /> : <FaEye className="h-5 w-5 text-gray-400" />}
+        </button>}
+      </div>
       <div className="label">
         <span onClick={(event) => event?.preventDefault()} className={`label-text-alt ${bTxtRightStyle}`}>{bottomTextRight}</span>
         <span onClick={(event) => event?.preventDefault()} className={`label-text-alt ${bTxtLeftStyle}`}>{bottomTextLeft}</span>
