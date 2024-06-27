a615c46f495b6f12dfdadb69eeb7541a555655c5
Petar Bojic <petar.bojic@quantum-bit.eu>
2024-06-27 16:38:36 +0200

    changing main layout.tsx

--- app/[locales]/layout.tsx
+++ app/[locales]/layout.tsx
@@ -4,16 +4,19 @@ import "./globals.css";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { StoreProvider } from "@/store/StoreProvider";
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  locale: string;
+}
 
 const manrope = Manrope({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
   locale,
-}: Readonly<{
-  children: React.ReactNode;
-  locale: string;
-}>) {
+}: LayoutProps) {
 
   const messages = await getMessages();
 
